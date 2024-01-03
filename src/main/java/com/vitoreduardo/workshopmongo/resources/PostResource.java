ca9b57b796e7515312e23a9acd9549f5993b9449
Vitor221 <vitornascimento6650@gmail.com>
2024-01-03 16:29:43 -0300

    Obtendo post por id

---
+++ src/main/java/com/vitoreduardo/workshopmongo/resources/PostResource.java
@@ -0,0 +1,27 @@
+package com.vitoreduardo.workshopmongo.resources;
+
+import com.vitoreduardo.workshopmongo.domain.Post;
+import com.vitoreduardo.workshopmongo.dto.UserDTO;
+import com.vitoreduardo.workshopmongo.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService postService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post post = postService.findById(id);
+        return ResponseEntity.ok().body(post);
+    }
+
+}
---
+++ src/main/java/com/vitoreduardo/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.vitoreduardo.workshopmongo.services;
+
+import com.vitoreduardo.workshopmongo.domain.Post;
+import com.vitoreduardo.workshopmongo.domain.User;
+import com.vitoreduardo.workshopmongo.dto.UserDTO;
+import com.vitoreduardo.workshopmongo.repository.PostRepository;
+import com.vitoreduardo.workshopmongo.repository.UserRepository;
+import com.vitoreduardo.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id) {
+        Optional<Post> post = postRepository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+    }
+}
