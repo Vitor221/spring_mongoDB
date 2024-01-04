9a21fd64b98cdff147ff88a734b2aee57976da56
Vitor221 <vitornascimento6650@gmail.com>
2024-01-04 10:10:00 -0300

    Consulta com varios cliterios

--- src/main/java/com/vitoreduardo/workshopmongo/resources/PostResource.java
+++ src/main/java/com/vitoreduardo/workshopmongo/resources/PostResource.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,6 +31,22 @@ public class PostResource {
         text = URL.decodeParam(text);
         List<Post> list = postService.findByTitle(text);
 
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate)
+    {
+        text = URL.decodeParam(text);
+
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+
+        List<Post> list = postService.fullSearch(text, min, max);
+        
         return ResponseEntity.ok().body(list);
     }
 
